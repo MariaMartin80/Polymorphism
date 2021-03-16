675fc289db78654bef108fca709a4448da05a658
MariaMartin80 <77515936+MariaMartin80@users.noreply.github.com>
2021-03-16 18:14:53 -0400

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; // Nedded for MessageBox
+
+namespace Polymorphism
+{
+    class Animal
+    {
+        //Field
+        private string _species;
+
+        //constructor
+        public Animal(string species)
+        {
+            _species = species;
+        }
+        
+        // species property
+        public string Species
+        {
+            get { return _species; }
+            set { _species = value; }
+        }
+
+        //makesound method
+        public virtual void MakeSound()
+        {
+            MessageBox.Show("Grrrrrrr");
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Polymorphism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //The showanimal method accepts an animal
+        //object as an argument. It displays the object's
+        //species and calls its makesound method.
+        private void showAnimalInfo(Animal animal)
+        {
+            MessageBox.Show("Species: " + animal.Species);
+            animal.MakeSound();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createAnimalButton_Click(object sender, EventArgs e)
+        {
+            Animal myAnimal = new Animal("Regular animal");
+            showAnimalInfo(myAnimal);
+        }
+
+        private void createDogButton_Click(object sender, EventArgs e)
+        {
+            Dog myDog = new Dog("Fido");
+            MessageBox.Show("The dog's name is " + myDog.Name);
+            showAnimalInfo(myDog);
+        }
+
+        private void createCatButton_Click(object sender, EventArgs e)
+        {
+            Cat myCat = new Cat("Kitty");
+            MessageBox.Show("The cat's name is " + myCat.Name);
+            showAnimalInfo(myCat);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+    }
+}
